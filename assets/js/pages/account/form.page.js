60a457a761c1b21ed90fd6b3fd83d3b3a9b92573
Sean Warman <seanwarman@gmail.com>
2018-01-08 15:13:52 +0000

    added a new thankyou message upon form submission

--- assets/js/pages/account/form.page.js
+++ assets/js/pages/account/form.page.js
@@ -19,7 +19,8 @@ parasails.registerPage('form', {
     },
     formObject:   {
 
-    }
+    },
+    hidden: false
 
   },
 
@@ -66,9 +67,27 @@ parasails.registerPage('form', {
       await parasails.require('pause')(1000);
       this.syncing.status = false;
 
-      this.submitMessage = 'Your proposal must be manually reviewed by an administrator.  Once it\'s reviewed, it will be placed on Github where everyone is free to discuss it.  With enough supporters, it will then be approved and a chat room will be created for it.';
-      await parasails.require('pause')(10000);
-      this.submitMessage = '';
+      // this.submitMessage = 'Your proposal must be manually reviewed by an administrator.  Once it\'s reviewed, it will be placed on Github where everyone is free to discuss it.  With enough supporters, it will then be approved and a chat room will be created for it.';
+      // await parasails.require('pause')(10000);
+      // this.submitMessage = '';
+
+      if (this.formObject.status === 'pending' && !this.syncing.status) {
+
+        this.hidden = true;
+        this.submitMessage = 'Thank you for submitting your proposal to the Bitcoin Cash Fund. We aim to provide a response within 48 hours. You can track your proposal here... <br><br> <a href="https://github.com/The-Bitcoin-Cash-Fund/FPR/pulls">https://github.com/The-Bitcoin-Cash-Fund/FPR/pulls</a><br><br>In the meantime, join us in our live chat at <a href="https://chat.thebitcoincash.fund">https://chat.thebitcoincash.fund</a> to discuss your project and all things Bitcoin Cash.<br><br>See you in there!';
+        await parasails.require('pause')(12000);
+        this.submitMessage = '';
+        this.hidden = false;
+
+      } else if (this.formObject.status === 'draft' && !this.syncing.status) {
+
+        this.hidden = true;
+        this.submitMessage = '<center>How could you? :(</center>';
+        await parasails.require('pause')(2000);
+        this.submitMessage = '';
+        this.hidden = false;
+      }
+
 
     },
     syncRemote: async function(attributeName, newFormObject, oldFormObject) {
