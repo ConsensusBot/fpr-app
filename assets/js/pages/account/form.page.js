d6af7d978b47d17dbd31b944b129117ec16a8234
Sean Warman <seanwarman@gmail.com>
2018-01-21 16:59:49 +0000

    periferal work on form submission requirements

--- assets/js/pages/account/form.page.js
+++ assets/js/pages/account/form.page.js
@@ -20,10 +20,7 @@ parasails.registerPage('form', {
     formObject:   {
 
     },
-    hidden: false,
-    filledOrNot: '',
-    filledStatus: undefined
-
+    hidden: false
 
   },
 
@@ -44,52 +41,37 @@ parasails.registerPage('form', {
   //  ║║║║╣  ║ ╠═╣║ ║ ║║╚═╗
   //  ╩ ╩╚═╝ ╩ ╩ ╩╚═╝═╩╝╚═╝
   methods: {
-
     toggleSubmission: async function(newValue) {
 
-
-
-// ************
-      if (this.formObject.projectName && this.formObject.startDate && this.formObject.hashtag && this.formObject.stakeholders && this.formObject.projectSummary && this.formObject.resources && this.formObject.budget && this.formObject.timeline && this.formObject.goals) {
-        // console.log('filled in!');
-        this.filledStatus = true;
-      } else {
-        // console.log('not filled in!');
-        this.filledStatus = false;
-      }
-
-
-      if (this.formObject.status === 'draft' && (!this.filledStatus)) {
-        console.log('the form may NOT be submitted');
-        this.filledOrNot = 'You must fill all required sections of the form to submit';
-      } else if (this.formObject.status === 'draft' && (this.filledStatus)) {
-        console.log('it is filled');
-
-        //saving section TODO: tidy this up, could be a better way so that this saving code isn't written out twice **
-        var changes = {
+      var changes = {
         id: this.oldFormObject.id,
         status: newValue
-        };
+      };
 
-        this.syncing.status = true;
+      this.syncing.status = true;
 
-        var cloudError;
-        var serverResponseData = await Cloud.saveForm(changes)
-        .tolerate((err)=>{ cloudError = err; });
+      var cloudError;
+      var serverResponseData = await Cloud.saveForm(changes)
+      .tolerate((err)=>{ cloudError = err; });
 
-        if (cloudError) {
-          // (FUTURE: have a think re error handling here, and what you want to do, if anything)
-          throw err;
-        } else {
-          // console.log(`And re: "${attributeName}", the server says:`, serverResponseData);
-        }
+      if (cloudError) {
+        // (FUTURE: have a think re error handling here, and what you want to do, if anything)
+        throw err;
+      } else {
+        // console.log(`And re: "${attributeName}", the server says:`, serverResponseData);
+      }
 
-        _.extend(this.oldFormObject, serverResponseData);
-        _.extend(this.formObject, serverResponseData);
+      _.extend(this.oldFormObject, serverResponseData);
+      _.extend(this.formObject, serverResponseData);
 
-        await parasails.require('pause')(1000);
-        this.syncing.status = false;
-        //**
+      await parasails.require('pause')(1000);
+      this.syncing.status = false;
+
+      // this.submitMessage = 'Your proposal must be manually reviewed by an administrator.  Once it\'s reviewed, it will be placed on Github where everyone is free to discuss it.  With enough supporters, it will then be approved and a chat room will be created for it.';
+      // await parasails.require('pause')(10000);
+      // this.submitMessage = '';
+
+      if (this.formObject.status === 'pending' && !this.syncing.status) {
 
         this.hidden = true;
         this.submitMessage = 'Thank you for submitting your proposal to the Bitcoin Cash Fund. We aim to provide a response within 48 hours. You can track your proposal here... <br><br> <a href="https://github.com/The-Bitcoin-Cash-Fund/FPR/pulls">https://github.com/The-Bitcoin-Cash-Fund/FPR/pulls</a><br><br>In the meantime, join us in our live chat at <a href="https://chat.thebitcoincash.fund">https://chat.thebitcoincash.fund</a> to discuss your project and all things Bitcoin Cash.<br><br>See you in there!';
@@ -97,31 +79,7 @@ parasails.registerPage('form', {
         this.submitMessage = '';
         this.hidden = false;
 
-      } else if (this.formObject.status === 'pending')  {
-
-        var changes = {
-        id: this.oldFormObject.id,
-        status: newValue
-        };
-
-        this.syncing.status = true;
-
-        var cloudError;
-        var serverResponseData = await Cloud.saveForm(changes)
-        .tolerate((err)=>{ cloudError = err; });
-
-        if (cloudError) {
-          // (FUTURE: have a think re error handling here, and what you want to do, if anything)
-          throw err;
-        } else {
-          // console.log(`And re: "${attributeName}", the server says:`, serverResponseData);
-        }
-
-        _.extend(this.oldFormObject, serverResponseData);
-        _.extend(this.formObject, serverResponseData);
-
-        await parasails.require('pause')(1000);
-        this.syncing.status = false;
+      } else if (this.formObject.status === 'draft' && !this.syncing.status) {
 
         this.hidden = true;
         this.submitMessage = 'Your submission has been withdrawn.';
@@ -129,8 +87,9 @@ parasails.registerPage('form', {
         this.submitMessage = '';
         this.hidden = false;
       }
-    },
 
+
+    },
     syncRemote: async function(attributeName, newFormObject, oldFormObject) {
       console.log(attributeName,'has changed from', oldFormObject[attributeName], 'to', newFormObject[attributeName]);
 
@@ -162,6 +121,7 @@ parasails.registerPage('form', {
       this.$forceUpdate(); // (because vue.js doesn't seem to be aware of the change above)
 
     }
+
   }
 
 });
